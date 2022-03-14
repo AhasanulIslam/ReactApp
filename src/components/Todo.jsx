8737d7640614504cde008d9af44c2ac35e59512e
AhasanulIslam <bsse1024@iit.du.ac.bd>
2022-03-14 17:45:57 +0600

    text create and delete

---
+++ src/components/Todo.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const Todo = () => {
+  const [todoName, setTodoName] = useState([]);
+  const [text, setText] = useState("");
+
+  const saveTodo = () => {
+    setTodoName([
+      ...todoName,
+      {
+        id: Math.random(),
+        title: text,
+      },
+    ]);
+  };
+
+
+  const deleteTodo = value => {
+      console.log(value);
+      const updatedList = todoName.filter(el => el.id !== value)
+      console.log(updatedList);
+      setTodoName(updatedList)
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Enter your text here"
+        name="todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={saveTodo}>Add Todo</button>
+
+      {todoName.length == 0 ? (
+        <h1>Please add todo</h1>
+      ) : (
+        todoName.map((el) => (
+          <div key={el.id}>
+            <h1>{el.title}</h1>
+            <button onClick={() => deleteTodo(el.id)}>Delete</button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Todo;
